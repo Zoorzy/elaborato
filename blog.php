d78ba691e0726bd2f684ad8e63cb8d45b2580aa2
Zoorzy <64961650+Zoorzy@users.noreply.github.com>
2021-05-18 13:33:01 +0200

    piccolezze

--- blog.php
+++ blog.php
@@ -15,29 +15,16 @@ require("includes/header.php");
             <h2>Ultime notizie su di noi</h2>
           </header>
           <p>
-            Vehicula fermentum ligula at pretium. Suspendisse semper iaculis eros, eu aliquam
-            iaculis. Phasellus ultrices diam sit amet orci lacinia sed consequat.
+            Qui trovi le nostre ultime apparizioni sulla rete
           </p>
           <ul class="link-list">
-            <li><a href="#">Sed dolore viverra</a></li>
-            <li><a href="#">Ligula non varius</a></li>
-            <li><a href="#">Dis parturient montes</a></li>
-            <li><a href="#">Nascetur ridiculus</a></li>
+            <li><a href="#">LaRepubblica</a></li>
+            <li><a href="#">Il Corriere della Sera</a></li>
+            <li><a href="#">RTL 102.5</a></li>
+            <li><a href="#">Il manifesto</a></li>
           </ul>
         </section>
-        <section>
-          <header>
-            <h2>Magna Phasellus</h2>
-          </header>
-          <ul class="link-list">
-            <li><a href="#">Sed dolore viverra</a></li>
-            <li><a href="#">Ligula non varius</a></li>
-            <li><a href="#">Nec sociis natoque</a></li>
-            <li><a href="#">Penatibus et magnis</a></li>
-            <li><a href="#">Dis parturient montes</a></li>
-            <li><a href="#">Nascetur ridiculus</a></li>
-          </ul>
-        </section>
+
       </div>
       <div class="col-6 col-12-medium imp-medium">
 
@@ -45,7 +32,7 @@ require("includes/header.php");
         <section>
           <header>
             <h2>Blog</h2>
-            <h3>A generic three column layout</h3>
+            <h3>Visita il nostro blog per restare sempre aggiornato!</h3>
           </header>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam metus, congue
@@ -56,56 +43,43 @@ require("includes/header.php");
             varius ac, lacinia sit amet augue. Nam ultrices elementum eros.
           </p>
           <p>
-            Sed faucibus viverra ligula, non varius magna semper vitae. Donec eu justo ut ipsum
-            hendrerit congue nec eu risus. Cum sociis natoque penatibus et magnis dis parturient
-            montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing
-            egestas tempus. Cras convallis odio sit amet risus convallis porttitor. Integer
-            vehicula fermentum ligula at pretium. Suspendisse semper iaculis eros, eu aliquam
-            justo imperdiet vel. Proin nec dictum mi. Duis commodo enim non tellus interdum
-            elit. Suspendisse fermentum adipiscing nisi, a tempor libero malesuada at. Morbi
-            lacinia dui adipiscing risus eleifend tincidunt. Proin eu mauris eu tellus eleifend
-            hendrerit.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam metus, congue
+            vel suscipit ut, dignissim non risus. Vestibulum ante est, fringilla nec placerat
+            eu, vestibulum vitae diam. Integer eget egestas eros. Duis enim erat, mollis quis
+            lacinia eget, blandit nec ipsum. Donec vitae turpis ipsum. Aliquam mauris libero,
+            sagittis in eleifend at, mattis imperdiet velit. Donec urna risus, rutrum molestie
+            varius ac, lacinia sit amet augue. Nam ultrices elementum eros.
           </p>
           <p>
-            Mauris sit amet tellus urna. In facilisis, tortor vitae ultricies egestas, odio
-            mi rhoncus arcu, quis euismod felis felis et velit. Mauris varius consectetur erat
-            egestas tempus. Cras convallis odio sit amet risus convallis porttitor. Integer
-            vehicula fermentum ligula at pretium. Suspendisse semper iaculis eros, eu aliquam
-            justo imperdiet vel. Proin nec dictum mi. Duis commodo enim non tellus interdum
-            iaculis. Phasellus ultrices diam sit amet orci lacinia sed consequat dui auctor.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam metus, congue
+            vel suscipit ut, dignissim non risus. Vestibulum ante est, fringilla nec placerat
+            eu, vestibulum vitae diam. Integer eget egestas eros. Duis enim erat, mollis quis
+            lacinia eget, blandit nec ipsum. Donec vitae turpis ipsum. Aliquam mauris libero,
+            sagittis in eleifend at, mattis imperdiet velit. Donec urna risus, rutrum molestie
+            varius ac, lacinia sit amet augue. Nam ultrices elementum eros.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam metus, congue
+            vel suscipit ut, dignissim non risus. Vestibulum ante est, fringilla nec placerat
+            eu, vestibulum vitae diam. Integer eget egestas eros. Duis enim erat, mollis quis
+            lacinia eget, blandit nec ipsum. Donec vitae turpis ipsum. Aliquam mauris libero,
+            sagittis in eleifend at, mattis imperdiet velit. Donec urna risus, rutrum molestie
+            varius ac, lacinia sit amet augue. Nam ultrices elementum eros.
           </p>
         </section>
 
       </div>
       <div class="col-3 col-12-medium">
 
-        <!-- Right Sidebar -->
         <section>
           <header>
-            <h2>Magna Phasellus</h2>
+            <h2>Pagine social</h2>
           </header>
           <ul class="link-list">
-            <li><a href="#">Sed dolore viverra</a></li>
-            <li><a href="#">Ligula non varius</a></li>
-            <li><a href="#">Nec sociis natoque</a></li>
-            <li><a href="#">Penatibus et magnis</a></li>
-            <li><a href="#">Dis parturient montes</a></li>
-            <li><a href="#">Nascetur ridiculus</a></li>
-          </ul>
-        </section>
-        <section>
-          <header>
-            <h2>Ipsum Dolor</h2>
-          </header>
-          <p>
-            Vehicula fermentum ligula at pretium. Suspendisse semper iaculis eros, eu aliquam
-            iaculis. Phasellus ultrices diam sit amet orci lacinia sed consequat.
-          </p>
-          <ul class="link-list">
-            <li><a href="#">Sed dolore viverra</a></li>
-            <li><a href="#">Ligula non varius</a></li>
-            <li><a href="#">Dis parturient montes</a></li>
-            <li><a href="#">Nascetur ridiculus</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">YouTube</a></li>
           </ul>
         </section>
 
